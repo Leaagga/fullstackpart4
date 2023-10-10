a307e7840a8e7c37186c5364d88fd4c9b7552abe
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-10 16:38:45 +0800

    4a 4.5

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -6,4 +6,7 @@ const dummy = (blogs) => {
 const totalLikes = (blogsList) => {
     return blogsList.reduce((acc, bitem) => acc+bitem.likes, 0)
 }
-module.exports={ dummy,totalLikes }
+const favoriteBlog = (favoriteBlogList)=> {
+    return favoriteBlogList.reduce((acc, cur)=>cur.likes>acc.likes?cur:acc,favoriteBlogList[0])
+}
+module.exports={ dummy,totalLikes,favoriteBlog }
