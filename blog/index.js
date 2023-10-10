8d1bfa71cd6adb041ec117c433859ffec6d482b9
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-10 12:13:18 +0800

    4.2 Router

--- blog/index.js
+++ blog/index.js
@@ -2,9 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter=require('./comtrollers/blog')
 const mongoose = require('mongoose')
 const config=require("./utils/config")
-const Blog=require('./models/blog')
+
 
 
 
@@ -13,24 +14,8 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs',blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
