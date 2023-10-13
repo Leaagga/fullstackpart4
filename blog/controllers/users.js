f41455641696af72c696be8b6b930d80572eb9d0
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-13 21:10:30 +0800

    4d 4.16

--- blog/controllers/users.js
+++ blog/controllers/users.js
@@ -4,17 +4,27 @@ const User = require('../models/user')
 
 usersRouter.post('/',async (request,response) => {
     const { username, name, password }=request.body
+    if (password.length<3){
+        return response.status(400).json('password is shorter than the minimum allowed length (3).')
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password,saltRounds)
     const newUser = new User({
-        username,
-        name,
-        passwordHash
+        username:username,
+        name:name,
+        passwordHash:passwordHash
     })
-    const savedUser = await newUser.save()
+    
+    try{
+        const savedUser = await newUser.save()
+        response.status(201).json(savedUser)
+    }catch(error){
+        console.log(error)
+        response.status(400).json(error.message)
+    }
 
-    response.status(201).json(savedUser)
+    
 })
 usersRouter.get('/',async (request,response) => {
     const user = await User.find({})
