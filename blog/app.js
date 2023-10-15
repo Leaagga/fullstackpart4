d7f5cf204ec2a825c2b7eca3060e0347ef802e79
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-15 16:38:47 +0800

    4d 4.18

--- blog/app.js
+++ blog/app.js
@@ -5,6 +5,7 @@ const blogsRouter=require('./controllers/blog')
 const mongoose = require('mongoose')
 const config=require("./utils/config")
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -15,4 +16,5 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs',blogsRouter)
 app.use('/api/users',usersRouter)
+app.use('/api/login',loginRouter)
 module.exports = app
